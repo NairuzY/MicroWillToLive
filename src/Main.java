889c3ac0fe7fd35f8e0c3b8c97e88709db1d05c0
SaraAlajmy <saraalagami@gmail.com>
2023-12-01 16:46:44 +0200

    start method

--- src/Main.java
+++ src/Main.java
@@ -6,43 +6,73 @@ import java.util.*;
 public class Main {
 
     int cycle = 0;
+    Instruction toBeIssued; 
+    int addLatency;
+    int subLatency;
+    int multLatency;
+    int divLatency;
+    int loadLatency;
+    int storeLatency;
+    int addReservationStations;
+    int multReservationStations;
+    int loadBuffer;
+    int storeBuffer;
+    Add[] addReservationStation;
+    Multiply[] multReservationStation;
+    Load[] loadReservationStation;
+    Store[] storeReservationStation;
+
+    public static void fetch(String instruction) {
+        if(toBeIssued == null) {
+
+            // fetch instruction from memory
+            // decode instruction
+            // create instruction object
+        }
+    }
+
+    public static void start() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of cycles for add instructions:");
+        addLatency = sc.nextInt();
+        System.out.println("Enter the number of cycles for subtract instructions:");
+        subLatency = sc.nextInt();
+        System.out.println("Enter the number of cycles for multiply instructions:");
+        multLatency = sc.nextInt();
+        System.out.println("Enter the number of cycles for divide instructions:");
+        divLatency = sc.nextInt();
+        System.out.println("Enter the number of cycles for load instructions:");
+        loadLatency = sc.nextInt();
+        System.out.println("Enter the number of cycles for store instructions:");
+        storeLatency = sc.nextInt();
+        System.out.println("Enter the size of the add/sub reservation station:");
+        addReservationStations = sc.nextInt();
+        System.out.println("Enter the size of the multiply/divide reservation station:");
+        multReservationStations = sc.nextInt();
+        System.out.println("Enter the size of the load buffer:");
+        loadBuffer = sc.nextInt();
+        System.out.println("Enter the size of the store buffer:");
+        storeBuffer = sc.nextInt();
+
+        addReservationStation = new Add[addReservationStations];
+        multReservationStation = new Multiply[multReservationStations];
+        loadReservationStation = new Load[loadBuffer];
+        storeReservationStation = new Store[storeBuffer];
+
+    }
     
     public static void main(String[] args) throws IOException {
 
+        start();
         File file = new File("program.txt");
         BufferedReader br = new BufferedReader(new FileReader(file));
         String instruction;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of cycles for add instructions:");
-        int addLatency = sc.nextInt();
-        System.out.println("Enter the number of cycles for subtract instructions:");
-        int subLatency = sc.nextInt();
-        System.out.println("Enter the number of cycles for multiply instructions:");
-        int multLatency = sc.nextInt();
-        System.out.println("Enter the number of cycles for divide instructions:");
-        int divLatency = sc.nextInt();
-        System.out.println("Enter the number of cycles for load instructions:");
-        int loadLatency = sc.nextInt();
-        System.out.println("Enter the number of cycles for store instructions:");
-        int storeLatency = sc.nextInt();
-        System.out.println("Enter the size of the add/sub reservation station:");
-        int addReservationStations = sc.nextInt();
-        System.out.println("Enter the size of the multiply/divide reservation station:");
-        int multReservationStations = sc.nextInt();
-        System.out.println("Enter the size of the load buffer:");
-        int loadBuffer = sc.nextInt();
-        System.out.println("Enter the size of the store buffer:");
-        int storeBuffer = sc.nextInt();
-        
-        // Reservation stations
-        Add[] addReservationStation = new Add[addReservationStations];
-        Multiply[] multReservationStation = new Multiply[multReservationStations];
-        Load[] loadReservationStation = new Load[loadBuffer];
-        Store[] storeReservationStation = new Store[storeBuffer];
-        
         while ((instruction = br.readLine()) != null) {
         
         }
+
+
     }
     
     
