0893482eaaca1ef2e38a5697786137c108839c9b
Nairuz yasser <84462600+NairuzY@users.noreply.github.com>
2023-12-01 20:32:43 +0200

    added setters and getter for each reservation station (#2)

--- src/ReservationStations/Add.java
+++ src/ReservationStations/Add.java
@@ -1,20 +1,63 @@
 package ReservationStations;
 
-public class Add implements ReservationStation {
-    
-    Opcode op;
-    int Vj;
-    int Vk;
-    int Qj;
-    int Qk;
+import Instructions.Instruction;
 
-    public Add() {
-        super();
+public class Add extends ReservationStation {
+
+    private Float Vj;
+    private Float Vk;
+    private String Qj;
+    private String Qk;
+
+    public Add(String tag) {
+
+        super(tag);
+
+        this.Vj = null;
+        this.Vk = null;
+        this.Qj = null;
+        this.Qk = null;
+
     }
-    
-    
-    @Override
-    public void execute() {
-    
+
+    public void setValues(Float Vj, Float Vk, String Qj, String Qk, Instruction instruction) {
+        this.Vj = Vj;
+        this.Vk = Vk;
+        this.Qj = Qj;
+        this.Qk = Qk;
+        this.instruction = instruction;
     }
+
+    public void setVj(Float Vj) {
+        this.Vj = Vj;
+    }
+
+    public void setVk(Float Vk) {
+        this.Vk = Vk;
+    }
+
+    public void setQj(String Qj) {
+        this.Qj = Qj;
+    }
+
+    public void setQk(String Qk) {
+        this.Qk = Qk;
+    }
+
+    public Float getVj() {
+        return Vj;
+    }
+
+    public Float getVk() {
+        return Vk;
+    }
+
+    public String getQj() {
+        return Qj;
+    }
+
+    public String getQk() {
+        return Qk;
+    }
+
 }
