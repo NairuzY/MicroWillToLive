94b87b21135b64b084cb0dc1d815af2dd0e7c7c9
SaraAlajmy <saraalagami@gmail.com>
2023-12-08 15:24:38 +0200

    fixes in found errors

--- src/Storage/Memory.java
+++ src/Storage/Memory.java
@@ -1,16 +1,16 @@
 package Storage;
 
 public class Memory {
-    
+
     int n;
     public static float[] values;
-    
+
     public Memory(int n) {
         values = new float[n];
-        for (int i=0;i<values.length;i++)
-        values[i]=i+1;
+        for (int i = 0; i < values.length; i++)
+            values[i] = i + 1;
     }
-    
+
     public static void print() {
         for (int i = 0; i < values.length; i++) {
             System.out.println("Memory " + i + " value " + values[i]);
